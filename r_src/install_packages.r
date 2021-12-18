5706e2849bda263a5e8e7e33b0238dd07d1b8183
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-12-18 00:57:49 -0800

    Added script to install packages

---
+++ r_src/install_packages.r
@@ -0,0 +1,25 @@
+##########################################################
+##                                                      ##
+## This file install necessary packages for the project ##
+##                                                      ##
+##########################################################
+
+
+
+## Install packages from CRAN ####
+## `shiny` - Package for the shiny app
+## `shinyWidgets` - Package for additional widgets for the app
+## `plotly` - Package for interactive plotting
+
+cran_packages <- c("shiny",
+                   "shinyWidgets",
+                   "plotly")
+if (length(missing_pkgs <- setdiff(cran_packages, rownames(installed.packages()))) > 0) {
+  message("Installing missing package(s): ", 
+          paste(missing_pkgs, collapse = ", "))
+  install.packages(missing_pkgs, 
+                   repos = "https://cloud.r-project.org")
+} else {
+  message("All CRAN packages already installed!")
+}
+rm(cran_packages, missing_pkgs)
