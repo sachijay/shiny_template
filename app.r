2b67a8b9b660efea2fa64017b331c07414f3ce3d
Pramoda Sachinthana Jayasinghe <30071227+sachijay@users.noreply.github.com>
2021-12-18 01:44:21 -0800

    Added shinythemes to package install list

--- app.r
+++ app.r
@@ -17,12 +17,58 @@ library(shinyWidgets)
 ## Define the UI ####
 ui <- fluidPage(
   
+  ## Horizontal rule
+  hr(),
+  
+  ## App header 
+  fluidRow(
+    
+    ## App title
+    column(10,
+           titlePanel(title = tail(strsplit(here::here(), split = "/")[[1]], n = 1))
+    ),
+    
+    ## Dark mode toggle
+    column(2,
+           panel(
+             materialSwitch(
+               inputId = "dark_toggle",
+               label = icon("moon"), 
+               value = TRUE,
+               status = "default"
+             )
+           )
+    )
+    
+  ),
+  
+  ## Horizontal rule
+  hr(),
+  
+  # theme = shinythemes::shinytheme("darkly"),
+  mainPanel(
+    tabsetPanel(
+      type = "tabs",
+      tabPanel(
+        title = "Table",
+        icon = icon("table"),
+        tags$br(),
+        DT::DTOutput("table")
+      )
+    )
+    # checkboxInput(
+    #   inputId = "themeToggle",
+    #   label = icon("sun")
+    # )
+  )
 )
 
 
 # Define server logic ####
 server <- function(input, output) {
-  
+  output$table <- DT::renderDT({
+    iris
+  })
 }
 
 
--- r_src/install_packages.r
+++ r_src/install_packages.r
@@ -9,11 +9,13 @@
 ## Install packages from CRAN ####
 ## `shiny` - Package for the shiny app
 ## `shinyWidgets` - Package for additional widgets for the app
+## `shinythemes` - Themes for the shiny app
 ## `plotly` - Package for interactive plotting
 ## `here` - Easy file referencing
 
 cran_packages <- c("shiny",
                    "shinyWidgets",
+                   "shinythemes",
                    "plotly",
                    "here")
 if (length(missing_pkgs <- setdiff(cran_packages, rownames(installed.packages()))) > 0) {
